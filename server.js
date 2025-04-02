12611764c52939e0d8b0bf4874ac6a906f7fbf4c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-04-01 18:16:03 -0700

    Initial commit for comedi.ai site

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Simple server that only serves the index.html file
+  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end(`Error: ${err.message}`);
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
+
+const PORT = 8080;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)); 
